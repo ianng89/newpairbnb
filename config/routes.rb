6cb5556a554f4b5271a3fc8876e0f8015c3f1896
ianng89 <me@ian.ng>
2016-01-14 19:47:43 +0800

    listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  resources :listings
   resources :users
   root 'users#index'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resource :home, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
